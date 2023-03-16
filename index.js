b4e3de8cd61d32a3a1dad894424fcded24a5c0f3
sandra <nyamweasandra@gmail.com>
2023-03-16 21:04:41 +0300

    Completed assingnment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = "Sandra Nyamwea";
+
+const welcomeMessage = 'Welcome to Flatbook, ${"Sandra Nyamwea"}!';
+const excitedWelcomeMessage =welcomeMessage.toUpperCase();
+const shortGreeting = 'Welcome,${"Sandra Nyamwea".slice(0,1)}!';
